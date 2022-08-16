d1a1abfbbaaf211f35490914d43dda7c4d1053d6
hckuo145 <hck0821@gmail.com>
2022-08-17 00:48:45 +0800

    updata from 140.109.135.212 (local)

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 title=SENet_MSTFT_ASP
 
-cmd="--title $title --device cuda --batch 10        \
+cmd="--title ${title} --device cuda --batch 10      \
      --model_conf config/model/SENet_MSTFT_ASP.yaml \
      --hyper_conf config/hyper/tiny_dataset.yaml    \
-     --params exp/$title/ckpt/best_valid_eer.pt"
+     --params exp/${title}/ckpt/best_valid_eer.pt"
 
-mkdir -p exp/$title
-logfile=exp/$title/history.log
+mkdir -p exp/${title}
+logfile=exp/${title}/history.log
 
-python main.py --train $cmd > $logfile 
-python main.py --test  $cmd 
+python main.py --train ${cmd} > ${logfile}
+python main.py --test  ${cmd} 
 # python main.py --eval  $cmd 
